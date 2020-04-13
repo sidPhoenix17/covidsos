fa26716c97ef590fc4d0e6ff19ad0932d71a63fb
saloni.kathuria <saloni.kathuria@olx.com>
2020-04-13 20:22:54 +0530

    url accept page added to config

--- src/config/config.js
+++ src/config/config.js
@@ -31,6 +31,7 @@ const getConfig = () => {
     getVerifyRequest: baseUrl + '/verify_request_page',
     verifyRequest: baseUrl + '/verify_request',
     successStories: baseUrl + '/success_stories',
+    requestAcceptance: baseUrl + '/accept_page',
 
     accessTypeStorageKey: 'access_level',
     userNameStorageKey: 'username',

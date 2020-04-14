6cd54dcd87dadd1929d26a5a87adb48bc2228554
Chirag Bansal <chirag.bansal@rivigo.com>
2020-04-14 22:28:32 +0530

    Useful links page

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -21,7 +21,6 @@ import {Link, NavLink as NavLinkRRD} from "react-router-dom";
 // nodejs library to set properties for components
 import {PropTypes} from "prop-types";
 import UserDropDown from "components/DropDown/UserDropDown.js";
-import config from "config/config";
 // reactstrap components
 import {
   Col,
@@ -150,9 +149,10 @@ class Sidebar extends React.Component {
               </div>
               {/* Navigation */}
               <Nav navbar>
-                {this.getNavLink('/contact_us', 'fas fa-hands-helping text-green',
+                {this.getNavLink('/contact-us', 'fas fa-hands-helping text-green',
                     'Contact Us')}
                 {this.getNavLink('/about', 'fas fa-users text-blue', 'About COVID SOS')}
+                {this.getNavLink('/useful-links', 'fas fa-link text-red', 'Useful Links')}
                 {/*{this.getNavLink('/pending-requests', 'fa fa-medkit', 'Pending Requests')}*/}
                 {this.getNavLink('/stories', 'fab fa-instagram', 'Volunteer Stories')}
                 {

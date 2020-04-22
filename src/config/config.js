c17bff6f9fac504fc489f3b0ca63164d9579d3f3
skathuria29 <kathuria.saloni29@gmail.com>
2020-04-23 00:55:37 +0530

    spurce list api added

--- src/config/config.js
+++ src/config/config.js
@@ -36,6 +36,7 @@ const getConfig = () => {
     resendOTP: baseUrl + '/resend_otp',
     verifytOTP: baseUrl + '/verify_otp',
     assignRequest: baseUrl + '/assign_request',
+    sourceList: baseUrl + '/source_list',
 
 
     accessTypeStorageKey: 'access_level',

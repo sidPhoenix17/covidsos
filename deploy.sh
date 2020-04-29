d507bdff819091f0f4b2a4e70a1838d603105ead
Ubuntu <ubuntu@ip-172-31-43-39.ap-south-1.compute.internal>
2020-04-29 13:35:28 +0530

    deploy for local stg deployment

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+url=https://stg.covidsos.org
+env=stg
+
+echo $url
+
+REACT_APP_ENV=$env npm run build
+cd build
+tar -zcvf build.tar.gz *
+echo "---------"
+rm -rf /var/www/html-new/*
+cp build.tar.gz /var/www/html-new/
+cd /var/www/html-new/ && tar -xvf build.tar.gz && rm build.tar.gz
+echo "---------"
+rm -rf /var/www/html/* && mv /var/www/html-new/* /var/www/html/
+echo "---------"
+
+echo ""
+echo ""
+echo "Link: "$url
+echo ""
+echo ""
+date

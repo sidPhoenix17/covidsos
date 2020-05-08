8387da4da8b20ab41cc0b026b52b423e07d8eae1
Saurabh Khandelwal <skhandelwal2@atlassian.com>
2020-05-09 01:11:52 +0530

    our parterns apge

--- src/views/OurPartners.js
+++ src/views/OurPartners.js
@@ -59,8 +59,8 @@ class About extends React.Component {
 
     renderPartnersCard(imgSrc, description, link){
         return (
-            <Col className="order-xl-2 mb-5 mb-xl-0" xl="4">
-            <Card className="card-profile shadow full-height-card">
+            <Col className="order-xl-2 mb-5 mb-xl-0" xl="4" >
+                <Card className="shadow border-0">
                 <CardHeader className="text-center mb-7">
                     <Row className="justify-content-center">
                         <Col className="m-auto">
@@ -87,28 +87,6 @@ class About extends React.Component {
         </Col>
 
         );
-    }
-    renderOrganisationCard(imgSrc, description, link) {
-
-        return this.ourPartnersDetails.map(partner => {this.renderPartnersCard(partner.imageSrc, partner.name, partner.link)});
-        // return (
-        //     <Col lg="10" md="10">
-        //         <Card className="shadow border-0">
-        //             <CardHeader className="pb-3">
-        //                 <div className="text-uppercase text-muted text-center mt-2 mb-2">
-        //                     <img alt='logo' src={imgSrc} style={{height: '10rem', maxWidth: '100%'}}/>
-        //                 </div>
-        //             </CardHeader>
-        //             <CardBody className="px-lg-5 py-lg-5 text-justify">
-        //                 <div className="text-justify mt-2 mb-2">{description}</div>
-        //             </CardBody>
-        //             <CardFooter className="py-4 text-right">
-        //                 <a href={link} className="btn btn-primary" target="_blank"
-        //                    rel="noopener noreferrer">Know More</a>
-        //             </CardFooter>
-        //         </Card>
-        //     </Col>
-        // );
     }
 
     sanitizeString(string) {
